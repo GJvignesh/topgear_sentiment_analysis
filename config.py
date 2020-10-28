a0d51849f9e5d94703b67549cd993f0abe0c92ff
Vignesh Gopal Jayalakshmi <vgopalja@cisco.com>
2020-10-28 09:49:21 +0530

    modify comfig

--- config.py
+++ config.py
@@ -5,6 +5,6 @@ VALID_BATCH_SIZE = 2
 EPOCHS = 1
 LEARNING_RATE = 1e-05
 
-checkpoint_path = r"/content/drive/My Drive/AGJCSV/models/distill_bert_32_unbalanced/pytorch_distilbert_31_unbalanced.pt"
-df_path = r"/content/drive/My Drive/AGJCSV/combained_validation_processed.csv"
-generic_path = r"/content/drive/My Drive/AGJCSV/models/distill_bert_32_unbalanced/"
+checkpoint_path = r"/content/drive/My Drive/AGJCSV/models/distill_bert_31_unbalanced/pytorch_distilbert_31_unbalanced.pt"
+df_path = r"/content/drive/My Drive/AGJCSV/combained_train_processed_new.csv"
+generic_path = r"/content/drive/My Drive/AGJCSV/models/distill_bert_31_unbalanced/"
