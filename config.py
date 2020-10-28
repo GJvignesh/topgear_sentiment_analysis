c41cc488f8e5e6a643f6592aed5cce588bb82c7a
Vignesh Gopal Jayalakshmi <vgopalja@cisco.com>
2020-10-28 17:22:19 +0530

    modify config

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 # Defining some key variables that will be used later on in the training
 MAX_LEN = 300
-TRAIN_BATCH_SIZE = 80
+TRAIN_BATCH_SIZE = 70
 VALID_BATCH_SIZE = 2
 EPOCHS = 1
 LEARNING_RATE = 1e-05
