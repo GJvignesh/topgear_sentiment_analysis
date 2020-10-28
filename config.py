bbb9ba3db3913e456a76f6e4f8a9ada62065e07b
Vignesh Gopal Jayalakshmi <vgopalja@cisco.com>
2020-10-28 09:07:32 +0530

    train dataset

--- config.py
+++ config.py
@@ -5,6 +5,7 @@ VALID_BATCH_SIZE = 250
 EPOCHS = 1
 LEARNING_RATE = 1e-05
 
-checkpoint_path = r"/content/drive/My Drive/AGJCSV/models/distill_bert_32_balanced/pytorch_distilbert_31_balanced.pt"
-df_path = r"/content/drive/My Drive/AGJCSV/combained_validation_processed.csv"
-generic_path = r"/content/drive/My Drive/AGJCSV/models/distill_bert_32_balanced/"
+checkpoint_path = r"/content/drive/My Drive/AGJCSV/models/distill_bert_31_balanced/pytorch_distilbert_31_balanced.pt"
+# df_path = r"/content/drive/My Drive/AGJCSV/combained_validation_processed.csv"
+df_path = r"/content/drive/My Drive/AGJCSV/combained_train_processed_new.csv"
+generic_path = r"/content/drive/My Drive/AGJCSV/models/distill_bert_31_balanced/"
