17855d783c623f0ea73b770c7889c5751f8af1c6
Vignesh Gopal Jayalakshmi <vgopalja@cisco.com>
2020-10-28 09:31:11 +0530

    modify comfig

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 # Defining some key variables that will be used later on in the training
-MAX_LEN = 512
-TRAIN_BATCH_SIZE = 25
-VALID_BATCH_SIZE = 250
+MAX_LEN = 300
+TRAIN_BATCH_SIZE = 80
+VALID_BATCH_SIZE = 2
 EPOCHS = 1
 LEARNING_RATE = 1e-05
 
-checkpoint_path = r"/content/drive/My Drive/AGJCSV/models/distill_bert_32_balanced/pytorch_distilbert_31_balanced.pt"
+checkpoint_path = r"/content/drive/My Drive/AGJCSV/models/distill_bert_32_unbalanced/pytorch_distilbert_31_unbalanced.pt"
 df_path = r"/content/drive/My Drive/AGJCSV/combained_validation_processed.csv"
-generic_path = r"/content/drive/My Drive/AGJCSV/models/distill_bert_32_balanced/"
+generic_path = r"/content/drive/My Drive/AGJCSV/models/distill_bert_32_unbalanced/"
