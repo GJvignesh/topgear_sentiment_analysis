d51ff11fc7ff18094caffec4e5a1a9212fcfffb5
Vignesh Gopal Jayalakshmi <vgopalja@cisco.com>
2020-11-05 01:36:44 +0530

    modify config

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
 # Defining some key variables that will be used later on in the training
-MAX_LEN = 64
+
+MAX_LEN = 64  # 6081 is more than 60 len out of 2238471 which is just 0.2%
 TRAIN_BATCH_SIZE = 16  # 8,16,32,64
 VALID_BATCH_SIZE = 16  # 8,16,32,64
 TEST_BATCH_SIZE = 16  # 8,16,32,64
@@ -8,7 +9,7 @@ LEARNING_RATE = 2e-05  # 5e-5, 3e-5, 2e-5
 PRE_TRAINED_MODEL_NAME = 'bert-base-cased'
 
 checkpoint_path = r"/content/drive/My Drive/AGJCSV/models/bert_31_1epoch/pytorch_bert_31_balanced.pt"
-df_path = r"C:/Users/vgopalja.PARTNERS/bert_multiclass/combained_train_processed_bert.csv"
+df_path = r"C:/Users/vgopalja.PARTNERS/bert_multiclass/comabined_bert_df.csv"  # (2244552, 2)
 generic_path = r"/content/drive/My Drive/AGJCSV/models/bert_31_1epoch/"
-df_valid_path = r"/content/drive/My Drive/AGJCSV/combained_validation_processed_bert.csv"
+# df_valid_path = r"/content/drive/My Drive/AGJCSV/combained_validation_processed_bert.csv"
 
