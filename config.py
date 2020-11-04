2f2a4c3383b048b626ade33b35bbd6d242545c38
Vignesh Gopal Jayalakshmi <vgopalja@cisco.com>
2020-11-04 16:54:18 +0530

    changing the files

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ LEARNING_RATE = 2e-05  # 5e-5, 3e-5, 2e-5
 PRE_TRAINED_MODEL_NAME = 'bert-base-cased'
 
 checkpoint_path = r"/content/drive/My Drive/AGJCSV/models/bert_31_1epoch/pytorch_bert_31_balanced.pt"
-df_path = r"C:/Users/vgopalja.PARTNERS/bert_multiclass/combained_train_processed_bert.csv"
+df_path = r"/content/drive/My Drive/AGJCSV/combained_train_processed_bert.csv"
 # df_path = r"/content/drive/My Drive/AGJCSV/combained_train_processed_new.csv"
 generic_path = r"/content/drive/My Drive/AGJCSV/models/bert_31_1epoch/"
 df_valid_path = r"/content/drive/My Drive/AGJCSV/combained_validation_processed_bert.csv"
