b1c222ba9bb6d543434c91a2b4eb2ac67960196c
Vignesh Gopal Jayalakshmi <vgopalja@cisco.com>
2020-12-01 12:46:16 +0530

    16 to 64

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 # Defining some key variables that will be used later on in the training
 
 MAX_LEN = 64  # 6081 datapoints are more than 60 length out of 2238471 which is just 0.2% of dataset
-TRAIN_BATCH_SIZE = 16  # 8,16,32,64
-VALID_BATCH_SIZE = 16  # 8,16,32,64
-TEST_BATCH_SIZE = 16  # 8,16,32,64
+TRAIN_BATCH_SIZE = 64  # 8,16,32,64
+VALID_BATCH_SIZE = 64  # 8,16,32,64
+TEST_BATCH_SIZE = 64  # 8,16,32,64
 EPOCHS = 3
 LEARNING_RATE = 2e-05  # 5e-5, 3e-5, 2e-5
 PRE_TRAINED_MODEL_NAME = 'distilbert-base-cased'
