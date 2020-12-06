eb08d5218185748e560f1a2eb4e4895b7f99b484
hacker <hackervignesh5@gmail.com>
2020-12-06 16:50:39 +0000

    adding balanced config

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 # Defining some key variables that will be used later on in the training
 
-MAX_LEN = 50  # 6081 datapoints are more than 60 length out of 2238471 which is just 0.2% of dataset
+MAX_LEN = 50  # 486 datapoints are more than 50 length out of 1488909 which is just 0.03264135014295702 %
 TRAIN_BATCH_SIZE = 16  # 8,16,32,64
 VALID_BATCH_SIZE = 16  # 8,16,32,64
 TEST_BATCH_SIZE = 16  # 8,16,32,64
@@ -11,13 +11,15 @@ PRE_TRAINED_MODEL_NAME = 'distilbert-base-cased'
 
 # checkpoint_path = r"/content/drive/My Drive/AGJCSV/models/distill_bert_31_unbalanced/pytorch_distilbert_31_unbalanced.pt"
 
+# Un-Balanced
+# checkpoint_path = r"/content/drive/My Drive/AGJCSV/models/distill_bert_31_unbalancedn/pytorch_distilbert_31_unbalanced_fulln.pt"
+# generic_path = r"/content/drive/My Drive/AGJCSV/models/distill_bert_31_unbalancedn/"
+# df_path = r"/content/drive/My Drive/AGJCSV/processed_new.csv"  # (1488909, 2)
 
-checkpoint_path = r"/content/drive/My Drive/AGJCSV/models/distill_bert_31_unbalancedn/pytorch_distilbert_31_unbalanced_fulln.pt"
-generic_path = r"/content/drive/My Drive/AGJCSV/models/distill_bert_31_unbalancedn/"
-df_path = r"/content/drive/My Drive/AGJCSV/processed_new.csv"  # (2244552, 2)
+# Balanced
+checkpoint_path = r"/content/drive/My Drive/AGJCSV/models/distill_bert_31_balancedn/pytorch_distilbert_31_balanced_fulln.pt"
+generic_path = r"/content/drive/My Drive/AGJCSV/models/distill_bert_31_balancedn/"
+df_path = r"/content/drive/My Drive/AGJCSV/processed_new.csv"  # (1488909, 2)
 
-
-# generic_path = r"/content/drive/My Drive/AGJCSV/models/bert_31_1epoch_unbalanced/"
-# df_valid_path = r"/content/drive/My Drive/AGJCSV/combained_validation_processed_bert.csv"
-
+df_valid_path = r"/content/drive/My Drive/AGJCSV/Validation_NEW/Validation_NEW.csv"
 
